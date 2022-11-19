45b8e7566327a254ab4b45bfa6f6e7dbb52fce20
Carmix <carmixdev@gmail.com>
2022-11-19 14:50:37 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,10 +5,12 @@ import os
 from setuptools import setup
 
 def read(fname):
-    # Utility function to read the README file
-    #   Used for the long_description.  It's nice, because now 1) we have a top level
-    #   README file and 2) it's easier to type in the README file than to put a raw
-    #   string in below ...
+    ''' 
+    Utility function to read the README file
+    Used for the long_description.  It's nice, because now 1) we have a top level
+    README file and 2) it's easier to type in the README file than to put a raw
+    string in below ...
+    '''
     return open(os.path.join(os.path.dirname(__file__), fname),encoding="utf-8").read()
 
 
